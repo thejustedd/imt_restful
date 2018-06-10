b3cc7be0399e1af30e4a6d490e9a55dab129d24a
Eduardo <eduard.kirsha@gmail.com>
2018-06-10 11:00:16 +0300

    ui modified

--- public/js/main.js
+++ public/js/main.js
@@ -1,98 +1,150 @@
 $(() => {
-	var get = () => {
+	const URL = '/space';
+	const TYPES_OF_SPACE_OBJS = [
+		'Звезда',
+		'Планета',
+		'Спутник планеты',
+		'Метеороид',
+		'Метеор',
+		'Комета',
+		'Астероид'
+	];
+
+	function getSpaceObjType(typeId) {
+		return TYPES_OF_SPACE_OBJS[typeId - 1];
+	}
+
+	let get = (url, fn) => {
 		$.ajax({
-			url: '/space',
+			url: url,
 			method: 'GET',
 			dataType: 'json',
-			success: data => {
-				console.log('GET');
-				var html = '<tr><th>ID</th><th>Name</th><th>Type</th></tr>';
-				data.forEach(elem => {
-					html += `<tr><th>${elem.id}</th><td>${elem.name}</td><td>${elem.type}</td></tr>`;
-				});
-				$('#table').html(html);
-			}
+			success: response => fn(response)
 		});
 	}
-
-	var head = () => {
+	let add = (url, data, fn) => {
 		$.ajax({
-			url: '/space',
-			method: 'HEAD',
-			dataType: 'json',
-			success: data => {
-				console.log('HEAD');
-				var html = '<tr><th>ID</th><th>Name</th><th>Type</th></tr>';
-				data.forEach(elem => {
-					html += `<tr><th>${elem.id}</th><td>${elem.name}</td><td>${elem.type}</td></tr>`;
-				});
-				$('#table').html(html);
-			}
-		});
-	}
-
-	var add = () => {
-		$.ajax({
-			url: '/space',
+			url: url,
 			method: 'POST',
 			dataType: 'json',
 			data: {
-				name: $('#nameAddBox').val(),
-				type: $('#typeAddBox').val()
+				name: data.name,
+				type: data.type
 			},
-			success: data => {
-				console.log('INSERT');
-				if (data.success) {
-					$('#nameAddBox').val('');
-					$('#typeAddBox').val('');
-					get();
-				}
-			}
+			success: response => fn(response)
 		});
 	};
-
-	var edit = () => {
+	let edit = (url, data, fn) => {
 		$.ajax({
-			url: '/space',
+			url: url,
 			method: 'PUT',
 			dataType: 'json',
 			data: {
-				id: $('#idEditBox').val(),
-				name: $('#nameEditBox').val(),
-				type: $('#typeEditBox').val()
+				id: data.id,
+				name: data.name,
+				type: data.type
 			},
-			success: data => {
-				console.log('UPDATE');
-				if (data.success) {
-					$('#idEditBox').val('');
-					$('#nameEditBox').val('');
-					$('#typeEditBox').val('');
-					get();
-				}
-			}
+			success: response => fn(response)
 		});
 	};
-
-	var del = () => {
+	let del = (url, data, fn) => {
 		$.ajax({
-			url: '/space',
+			url: url,
 			method: 'DELETE',
 			dataType: 'json',
 			data: {
-				id: $('#idDeleteBox').val()
+				id: data.id
 			},
-			success: data => {
-				console.log('DELETE');
-				if (data.success) {
-					$('#idDeleteBox').val('');
-					get();
-				}
-			}
+			success: response => fn(response)
 		});
 	};
 
-	get();
-	$('#addBtn').click(add);
-	$('#editBtn').click(edit);
-	$('#deleteBtn').click(del);
+	function getSuccess(res) {
+		let html = '<tr><th>Name</th><th>Type</th><th>Action</th></tr>';
+		res.forEach(elem => {
+			html += `<tr data-id="${elem.id}"><td>${elem.name}</td><td>${getSpaceObjType(elem.type)}</td><td><button class="btn btn-primary editBtn" data-id="${elem.id}" data-toggle="popover">Edit</button> <button class="btn btn-primary deleteBtn" data-id="${elem.id}">Delete</button></td></tr>`;
+		});
+		$('#table').html(html);
+	}
+	function postSuccess(res) {
+		let html = `<tr data-id=${res.id}><td>${res.name}</td><td>${getSpaceObjType(res.type)}</td><td><button class="btn btn-primary editBtn" data-id="${res.id}" data-toggle="popover">Edit</button> <button class="btn btn-primary deleteBtn" data-id="${res.id}">Delete</button></td></tr>`;
+		$('#table').append(html);
+		console.log(res);
+	}
+	function putSuccess(res) {
+		let tr = $('#table').find(`tr[data-id=${res.id}]`)[0];
+		tr.children[0].innerText = res.name;
+		tr.children[1].innerText = getSpaceObjType(res.type);
+		console.log(res);
+	}
+	function deleteSuccess(res) {
+		$('#table').find(`tr[data-id=${res.id}]`)[0].remove();
+		console.log(res);
+	}
+
+	get(URL, getSuccess);
+
+	$('#formAdd').submit((e) => {
+		let data = {
+			name: $('#nameAddBox').val(),
+			type: $('#typeAddBox').val()
+		};
+		$('#formAdd')[0].reset();
+
+		e.preventDefault();
+		add(URL, data, postSuccess);
+	});
+
+	$(document).popover({
+		trigger: 'click',
+		selector: '.editBtn',
+		placement: "top",
+		html: true,
+		content: `
+			<form class="form" id="formEdit">
+				<input class="form-control mb-2" name="name" id="nameEditBox" placeholder="Input name" required>
+				<select class="form-control mb-2" name="type" id="typeEditBox" required>
+					<option value="1">Звезда</option>
+					<option value="2">Планета</option>
+					<option value="3">Спутник планеты</option>
+					<option value="4">Метеороид</option>
+					<option value="5">Метеор</option>
+					<option value="6">Комета</option>
+					<option value="7">Астероид</option>
+				</select>
+				<button type="button" class="btn btn-primary confirmEditBtn" id="editBtn">OK</button>
+				<button type="reset" class="btn btn-primary">Clear</button>
+			</form>
+		`
+	});
+	$(document).on('click', '.editBtn', (e) => {
+		$('.editBtn').not(e.target).popover('dispose');
+
+		let tr = e.target.parentElement.parentElement;
+		let id = $(tr).data('id');
+		let name = tr.children[0].innerText;
+		let type = tr.children[1].innerText;
+
+		if (!$('#nameEditBox').val()) $('#nameEditBox').val(name);
+	});
+	$(document).on('click', '.confirmEditBtn', (e) => {
+		// e.preventDefault();
+
+		let popoverId = e.target.offsetParent.id;
+		let callBtn = $(`[aria-describedby=${popoverId}]`)[0];
+
+		let data = {
+			id: $(callBtn).data('id'),
+			name: $('#nameEditBox').val(),
+			type: $('#typeEditBox').val()
+		};
+
+		edit(URL, data, putSuccess);
+		$(callBtn).popover('dispose');
+	});
+	$(document).on('click', '.deleteBtn', (e) => {
+		let data = { id: $(e.target).data('id') };
+		let isConfirmed = confirm('Are you sure you want to delete this row [ID: ' + data.id + ']?');
+		if (isConfirmed) del(URL, data, deleteSuccess);
+	});
 });
--- server.js
+++ server.js
@@ -13,32 +13,34 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/main.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.route('/space')
 	.get((req, res) => {
 		dbPromise
 			.then(db => db.all('SELECT id, name, type FROM SpaceObjects'))
-			.then(spaceObjects => res.json(spaceObjects));
-	})
-	.head((req, res) => {
-		dbPromise
-			.then(db => db.all('SELECT id, name, type FROM SpaceObjects'))
-			.then(spaceObjects => res.json(spaceObjects));
+			.then(data => res.json(data));
 	})
 	.post((req, res) => {
 		var body = req.body;
-		dbPromise.then(db => db.get(`INSERT INTO SpaceObjects (name, type) VALUES ('${body.name}', '${body.type}')`));
-		res.json({ success: true });
+		dbPromise
+			.then(db => {
+				db.get(`INSERT INTO SpaceObjects (name, type) VALUES ('${body.name}', '${body.type}')`);
+				return db;
+			})
+			.then(db => db.get('SELECT last_insert_rowid() as id'))
+			.then(data => res.json({ id: data.id, name: body.name, type: +body.type }));
 	})
 	.put((req, res) => {
 		var body = req.body;
-		dbPromise.then(db => db.get(`UPDATE SpaceObjects SET name = '${body.name}', type = '${body.type}' WHERE ID = '${body.id}'`));
-		res.json({ success: true });
+		dbPromise
+			.then(db => db.get(`UPDATE SpaceObjects SET name = '${body.name}', type = '${body.type}' WHERE ID = '${body.id}'`))
+			.then(() => res.json({ id: +body.id, name: body.name, type: +body.type }));
 	})
 	.delete((req, res) => {
-		dbPromise.then(db => db.get(`DELETE FROM SpaceObjects WHERE ID = '${req.body.id}'`));
-		res.json({ success: true });
+		dbPromise
+			.then(db => db.get(`DELETE FROM SpaceObjects WHERE ID = '${req.body.id}'`))
+			.then(() => res.json({ id: +req.body.id }));
 	});
 app.listen('8080');
